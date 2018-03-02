0e461d2f766fc99b4ab6fd07d4b2c5368813a6b4
bond4u <raul.peldes@mail.ee>
2018-03-02 23:13:26 +0200

    new renamer dll sub-project

--- Renamer/Renamer.cs
+++ Renamer/Renamer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Text;//for Stringbuilder
 using Shoko.Commons.Extensions;
 using Shoko.Models.Enums;
@@ -14,6 +15,8 @@ namespace MyRenamer
   [Renamer("MyRenamer", Description = "My Renamer")]
   public class Renamer : IRenamer
   {
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+    
     //calls the other method
     //videolocal is unique file details in general
     //videolocal_place is instance of unique file somehwere in filesystem
@@ -22,76 +25,87 @@ namespace MyRenamer
     //so, file in general
     public string GetFileName(SVR_VideoLocal video)
     {
-      //get anidb info about the file
-      var file = video.GetAniDBFile();
-      //get first episode info
-      var episode = video.GetAnimeEpisodes()[0].AniDB_Episode;
-      //get anime info
-      var anime = RepoFactory.AniDB_Anime.GetByAnimeID(episode.AnimeID);
-      
       //file name we are putting together
       StringBuilder name = new StringBuilder();
-      
-      if (!string.IsNullOrWhiteSpace(file.Anime_GroupNameShort)) {
-        //have releasing group name
-        name.Append($"[{file.Anime_GroupNameShort}]");
-      }
-      //then episode name/title
-      name.Append($" {anime.PreferredTitle}");
-      
-      //what kind of file is it
-      if (anime.AnimeType == (int) AnimeType.Movie) {
-        //movie has a year
-        name.Append($" ({anime.BeginYear}) ");
-      } else { //it's not movie
-        string prefix = "";
-        if (anime.AnimeType == (int) AnimeType.OVA) {
-          prefix = "OVA";
-        } else { //not ova
-          if (episode.GetEpisodeTypeEnum() == EpisodeType.Credits) prefix = "C";
-          if (episode.GetEpisodeTypeEnum() == EpisodeType.Other) prefix = "O";
-          if (episode.GetEpisodeTypeEnum() == EpisodeType.Parody) prefix = "P";
-          if (episode.GetEpisodeTypeEnum() == EpisodeType.Special) prefix = "S";
-          if (episode.GetEpisodeTypeEnum() == EpisodeType.Trailer) prefix = "T";
+      try {
+        //try'n'get episodes
+        var eps = video.GetAnimeEpisodes();
+        if (null == eps) {
+          logger.Warn("MyRenamerDll: Episodes list is null");
+        } else if (0 == eps.Count) {
+          logger.Warn("MyRenamerDll: Episodes count is 0");
         }
-        int epCount = 1;
-        //how many episodes are there in total
-        if (episode.GetEpisodeTypeEnum() == EpisodeType.Episode) epCount = anime.EpisodeCountNormal;
-        if (episode.GetEpisodeTypeEnum() == EpisodeType.Special) epCount = anime.EpisodeCountSpecial;
-        //add eps/creds number & prepend zeroes
-        name.Append($" - {prefix}{PadNumberTo(episode.EpisodeNumber, epCount)}");
+        // get first episode
+        var episode = eps[0].AniDB_Episode;
+        //get anidb info about the file
+        var file = video.GetAniDBFile();
+        if (null == file) {
+          logger.Warn("MyRenamerDll: File is not known to AniDB");
+        }
+        if (!string.IsNullOrWhiteSpace(file.Anime_GroupNameShort)) {
+          //have releasing group name
+          name.Append($"[{file.Anime_GroupNameShort}]");
+        } else {
+          // try to guess group name from file name
+          if (file.FileName.StartsWith("[")) {
+            var idx = file.FileName.IndexOf("]", StringComparison.Ordinal);
+            if (0 <= idx) {
+              string grp = file.FileName.Substring(1, idx - 1); //ctrl+shift+space for overloads
+              logger.Warn("MyRenamerDll: 1possible group name: " + grp);
+              name.Append("[" + grp + "]");
+            }
+          } else if (file.FileName.StartsWith("(")) {
+            var idx = file.FileName.IndexOf(")", StringComparison.Ordinal);
+            if (0 <= idx) {
+              string grp = file.FileName.Substring(1, idx - 1);
+              logger.Warn("MyREnamerDll: 2possible group name: " + grp);
+              name.Append("[" + grp + "]");
+            }
+          }
+        }
+        //get anime info
+        var anime = RepoFactory.AniDB_Anime.GetByAnimeID(episode.AnimeID);
+        //then episode name/title
+        name.Append($" {anime.PreferredTitle}");
+        //what kind of file is it
+        if (anime.AnimeType == (int)AnimeType.Movie) {
+          //movie has a year
+          name.Append($" ({anime.BeginYear}) ");
+        } else { //it's not movie
+          string prefix = "";
+          if (anime.AnimeType == (int)AnimeType.OVA) {
+            prefix = "OVA";
+          } else { //not ova
+            if (episode.GetEpisodeTypeEnum() == EpisodeType.Credits) prefix = "C";
+            if (episode.GetEpisodeTypeEnum() == EpisodeType.Other) prefix = "O";
+            if (episode.GetEpisodeTypeEnum() == EpisodeType.Parody) prefix = "P";
+            if (episode.GetEpisodeTypeEnum() == EpisodeType.Special) prefix = "S";
+            if (episode.GetEpisodeTypeEnum() == EpisodeType.Trailer) prefix = "T";
+          }
+          int epCount = 1;
+          //how many episodes are there in total
+          if (episode.GetEpisodeTypeEnum() == EpisodeType.Episode) epCount = anime.EpisodeCountNormal;
+          if (episode.GetEpisodeTypeEnum() == EpisodeType.Special) epCount = anime.EpisodeCountSpecial;
+          //add eps/creds number & prepend zeroes
+          name.Append($" - {prefix}{PadNumberTo(episode.EpisodeNumber, epCount)}");
+        } //not movie
+        //is there a version
+        if (1 < file.FileVersion)
+          name.Append($"v{file.FileVersion}");
+        //is it censored
+        if (0 < file.IsCensored)
+          name.Append($" Censored");
+        //then add resolution
+        int height = Utils.GetVideoHeight(file.File_VideoResolution);
+        if (0 == height)
+          height = Utils.GetVideoHeight(video.VideoResolution);
+        name.Append($" [{height}p]"); //that's not very nice
+        name.Append($"[{video.CRC32.ToUpper()}]");
+        name.Append($"{System.IO.Path.GetExtension(video.FileName)}");
+      } catch (Exception ex) {
+        logger.Error(ex, "MyRenamerDll: Exception: " + ex.Message);
+        throw ex;
       }
-      
-      //is there a version
-      if (1<file.FileVersion)
-        name.Append($"v{file.FileVersion}");
-      //is it censored
-      if (0<file.IsCensored)
-        name.Append($" Censored");
-      //dont care about source
-      if (file.File_Source != null &&
-          (file.File_Source.Equals("DVD", StringComparison.InvariantCultureIgnoreCase) ||
-          file.File_Source.Equals("Blu-ray", StringComparison.InvariantCultureIgnoreCase))) {
-//        name.Append($" {file.File_Source}");
-      }
-      //then add resolution
-//      name.Append($" ({video.VideoResolution}");
-      int height = Utils.GetVideoHeight(file.File_VideoResolution);
-      if (0 == height)
-        height = Utils.GetVideoHeight(video.VideoResolution);
-      name.Append($" [{height}p]"); //that's not very nice
-      //video encoding
-//      name.Append($" {(file?.File_VideoCodec ?? video.VideoCodec).Replace("\\", "").Replace("/", "")}".TrimEnd());
-      //don't care about bits
-      if (video.VideoBitDepth == "10") {
-//        name.Append($" {video.VideoBitDepth}bit");
-      }
-      //resolution end
-//      name.Append(')');
-
-      name.Append($"[{video.CRC32.ToUpper()}]");
-      name.Append($"{System.IO.Path.GetExtension(video.FileName)}");
-      
       return Utils.ReplaceInvalidFolderNameCharacters(name.ToString());
     }
     
@@ -102,26 +116,46 @@ namespace MyRenamer
     
     //where does this file go
     public (ImportFolder dest, string folder) GetDestinationFolder(SVR_VideoLocal_Place video) {
+      ImportFolder dest = null;
       StringBuilder folder = new StringBuilder();
-      var anime = RepoFactory.AniDB_Anime.GetByAnimeID(video.VideoLocal.GetAnimeEpisodes()[0].AniDB_Episode.AnimeID);
-      bool IsPorn = anime.Restricted > 0;
-
-      //series file goes inside 'year/month/series' folder
-//      folder.Append($"{anime.BeginYear}/");
-      DateTime date = (DateTime)anime.AirDate;
-      if (null != date) {
-        if (1 < date.Year) {
-          folder.Append($"{date.Year}/");
-          if (0 < date.Month)
-            folder.Append($"{PadNumberTo(date.Month, 12)}/");
+      try {
+        var eps = video.VideoLocal.GetAnimeEpisodes();
+        if (null == eps) {
+          logger.Warn("MyRenamerDll: Episodes list is null");
+        } else if (0 == eps.Count) {
+                    logger.Warn("MyRenamerDll: Episodes list is empty");
         }
+        var anime = RepoFactory.AniDB_Anime.GetByAnimeID(eps[0].AniDB_Episode.AnimeID);
+        bool IsPorn = anime.Restricted > 0;
+        //series file goes inside 'year/month/series' folder
+        DateTime? date = anime.AirDate;
+        if (null != date) {
+          if (1 < date?.Year) {
+            folder.Append($"{date?.Year}");
+            folder.Append(System.IO.Path.DirectorySeparatorChar);
+            if (0 < date?.Month) {
+              folder.Append($"{PadNumberTo((int)date?.Month, 12)}");
+              folder.Append(System.IO.Path.DirectorySeparatorChar);
+            }
+          }
+        }
+        folder.Append($"{Utils.ReplaceInvalidFolderNameCharacters(anime.PreferredTitle)}");
+        bool isUnix = Environment.OSVersion.Platform.HasFlag(PlatformID.Unix);
+        string path = "";
+        if (isUnix) { // no drive letter in unix
+          path += System.IO.Path.DirectorySeparatorChar;
+          path += (IsPorn ? "_Hentai" : "_Animes");
+        } else {
+          path += "G" + System.IO.Path.VolumeSeparatorChar;
+          path += System.IO.Path.DirectorySeparatorChar;
+          path += "_torrentz" + System.IO.Path.DirectorySeparatorChar + "_sorted";
+        }
+        path += System.IO.Path.DirectorySeparatorChar;
+        dest = RepoFactory.ImportFolder.GetByImportLocation(path);
+      } catch (Exception ex) {
+        logger.Error("MyRenamerDll: Exception: " + ex.Message);
       }
-      folder.Append($"{Utils.ReplaceInvalidFolderNameCharacters(anime.PreferredTitle)}");
-
-      ImportFolder dest = RepoFactory.ImportFolder.GetByImportLocation(IsPorn ? "/_Hentai/" : "/_Animes/");
-//      ImportFolder dest = RepoFactory.ImportFolder.GetByImportLocation("/_Animes/");
-          
       return (dest, folder.ToString());
     }
-  }
-}
+  }//cls
+}//ns
